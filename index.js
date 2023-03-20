f39fedd08211c30f67f2d704c2cceaa0396b04b7
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-03-20 13:03:57 -0500

    update fonts

--- index.js
+++ index.js
@@ -73,6 +73,21 @@ function showTime() {
       break;
     case 6:
       monthy = 'July';
+      break;
+    case 7:
+      month = 'August';
+      break;
+    case 8:
+      month = 'September';
+      break;
+    case 9:
+      month = 'October';
+      break;
+    case 10:
+      month = 'November';
+      break;
+    case 11:
+      month = 'December';
   }
 
   document.getElementById('hours').innerHTML = hour;
